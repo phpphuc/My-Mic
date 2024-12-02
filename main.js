7bc53ef673d75c6dbce262a72119b32104a213bf
phpphuc <19522036@gmail.com>
2024-12-02 19:41:15 +0700

    implement speech recognition feature and separate styles into external CSS file

---
+++ main.js
@@ -0,0 +1,46 @@
+const content = document.getElementById("content");
+let recognition;
+
+function startRecognition() {
+  if ('webkitSpeechRecognition' in window) {
+    recognition = new webkitSpeechRecognition();
+    recognition.lang = 'en-US';
+    recognition.interimResults = true;
+    recognition.continuous = true;
+    recognition.maxAlternatives = 1;
+
+    var final_transcript = "";
+
+    recognition.onresult =  function(event) {
+      var interim_transcript = '';
+      for (var i = event.resultIndex; i < event.results.length; ++i) {
+        if (event.results[i].isFinal) {
+          final_transcript += event.results[i][0].transcript;
+        } else {
+          interim_transcript += event.results[i][0].transcript;
+        }
+      }
+      if (interim_transcript.length > 0) {
+        if (content) {
+          content.innerHTML = interim_transcript;
+        }
+      }
+
+
+    };
+    recognition.start();
+  } else {
+    alert('Trình duyệt của bạn không hỗ trợ Web Speech API');
+  }
+}
+
+document.getElementById('start-btn').addEventListener('click', () => {
+		startRecognition();
+});
+
+document.getElementById('stop-btn').addEventListener('click', () => {
+	if (recognition) {
+		recognition.stop();
+	}
+});
+
